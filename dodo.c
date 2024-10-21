230e07a37847f5031673f7ef4f31214902666065
pslow <mo.pr@outlook.com>
2024-10-21 13:14:38 +0200

    small changes

--- dodo.c
+++ dodo.c
@@ -1,3 +1,13 @@
+/**
+ * @file dodo.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-10-21
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 #include <stdio.h>
 int main() {
     char name [10];
