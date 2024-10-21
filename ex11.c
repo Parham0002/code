a46589b01850311e049948cfb8cae4cef1ed971f
pslow <mo.pr@outlook.com>
2024-10-21 13:12:00 +0200

    answering questions

--- ex11.c
+++ ex11.c
@@ -2,29 +2,28 @@
 #include <ctype.h>
 
 int main(){
-   char ch;
-   int state = 0;
+char ch;
+int state = 0;
 
-   printf("write your full name:");
+printf("write your full name:");
 
-  while((ch = getchar()) !=  '\n' ){
+while((ch = getchar()) !=  '\n' ){
 
-  if(isalpha(ch)){
-    if(!state){
-        putchar(toupper(ch));
-        state = 1;
-    } else {
-        putchar(tolower(ch));
-    }
-    }else if( ch == ' '){
-        if(state){
-            putchar(' ');
-            state = 0;
-        }
-
-    }
-  }
-  printf("\n");
+if(isalpha(ch)){
+if(!state){
+putchar(toupper(ch));
+state = 1;
+} else {
+putchar(tolower(ch));
+}
+}else if( ch == ' '){
+if(state){
+putchar(' ');
+state = 0;
+}
+}
+}
+printf("\n");
 
 return 0;
-  }
+}
--- ex2.c
+++ ex2.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h> 
 
 #define name "Parham Rezaei"
