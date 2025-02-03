4e2aa99760af41a3a40eac466d3499dca5604389
pslow <mo.pr@outlook.com>
2025-02-03 10:15:15 +0100

    continous

--- c++/exercises/ex1.cpp
+++ c++/exercises/ex1.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
+
 int main(void)
 {
 
 #if !defined(NUMBER) || ((NUMBER != 8) && (NUMBER != 16))
-#error error number must be 8 or 16
+// #error error number must be 8 or 16
 #else
     std::cout << "the value of number is:" << NUMBER << std::endl;
 #endif
